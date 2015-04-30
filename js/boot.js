963c0e53de4ae66ef186bbdb370a0ec491960f29
Junwen Qin <jwqin@thoughtworks.com>
2015-04-30 09:54:53 +0800

    test js

---
+++ js/boot.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jwqin on 4/30/15.
+ */
+(function(){
+    React.render(
+        React.createElement("em", null,  "thoughtworks"),
+        document.getElementById("h1")
+    );
+})();
